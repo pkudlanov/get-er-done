cca91218091dd724075a0364183af471a3e77f0d
Pavel Kudlanov <pkudlanov@yahoo.com>
2021-02-16 15:29:46 -0500

    Created a Todo Model and changed the view in admin

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created', 'completed', 'important')
+    readonly_fields = ('created',)
+
+
+admin.site.register(Todo, TodoAdmin)
