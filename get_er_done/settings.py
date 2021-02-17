ab14f13cc45b783ad5b3f460626c0b53bdc1bde0
Pavel Kudlanov <pkudlanov@yahoo.com>
2021-02-17 01:42:38 -0500

    removed login and logout redirect

--- get_er_done/settings.py
+++ get_er_done/settings.py
@@ -122,8 +122,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/home/'
-LOGOUT_REDIRECT_URL = '/home/'
 
 
 try:
